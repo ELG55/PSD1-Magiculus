72269c6d23ba14ef85aa45b9b770636df25356c1
Sekler <el_mm5mm@hotmail.com>
2021-02-13 16:54:08 -0700

    Creación del proyecto y detalles del menú inicial.

---
+++ Assets/Scripts/Savedata.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Savedata : MonoBehaviour
+{
+    //Save variables
+    public int slot;
+    public string mageName;
+    public int level;
+    public string mageClass;
+    public string date;
+    public string progress;
+    public float dmgDone;
+    public float dmgReceived;
+    public float hitP;
+
+    private static Savedata saveInstance;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (saveInstance == null)
+        {
+            saveInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject); // Used Destroy instead of DestroyObject
+        }
+    }
+}
---
+++ Assets/Scripts/TutorialSlide.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSlide : MonoBehaviour
+{
+    public Sprite image;
+    public string text;
+}
