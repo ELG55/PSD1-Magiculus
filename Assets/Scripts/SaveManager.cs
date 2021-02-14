41033f7eb15bd8df190820f369cb158a69b148ab
Dinamo28 <bruno_28_12@hotmail.com>
2021-02-13 22:24:11 -0700

    CargarCanvas P2

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -6,6 +6,33 @@ public static class SaveManager {
 
     public static void Salvar(Savedata sv)
     {
+        BinaryFormatter formatter = new BinaryFormatter();
         string path = Application.persistentDataPath + "/file1.qlo";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        Savedata data = sv;
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static Savedata Cargar()
+    {
+        string path = Application.persistentDataPath + "/file1.qlo";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            Savedata sv = formatter.Deserialize(stream) as Savedata;
+            stream.Close();
+
+            return sv;
+        }
+        else
+        {
+            Debug.LogError("save error not found in: " + path);
+            return null;
+        }
     }
 }
--- Assets/Scripts/Savedata.cs
+++ Assets/Scripts/Savedata.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Savedata : MonoBehaviour
 {
+
     //Save variables
     public int slot;
     public string mageName;
@@ -15,7 +17,65 @@ public class Savedata : MonoBehaviour
     public float dmgReceived;
     public float hitP;
 
-    private static Savedata saveInstance;
+    private Savedata saveInstance;
+
+    public GameObject name;
+    public GameObject nivel;
+    public GameObject clase;
+    public GameObject fecha;
+    public GameObject progreso;
+    public GameObject realizado;
+    public GameObject recibido;
+    public GameObject porcentaje;
+
+
+    public void SalvarDatos()
+    {
+        SaveManager.Salvar(this);
+    }
+    public void CargarDatos()
+    {
+        Savedata data = SaveManager.Cargar();
+
+        slot = data.slot;
+        mageName = data.mageName;
+        level = data.level;
+        mageClass = data.mageClass;
+        date = data.date;
+        progress = data.progress;
+        dmgDone = data.dmgDone;
+        dmgReceived = data.dmgReceived;
+        hitP = data.hitP;
+    }
+    public void NewGame()
+    {
+        slot = 1;
+        mageName = "0ofM4ster69";
+        level = 1;
+        mageClass = "Jiler XD";
+        date = "Hoy";
+        progress = ".-.XD";
+        dmgDone = 0;
+        dmgReceived = 0;
+        hitP = 0;
+
+        RefreshData();
+    }
+    public void LevelUp()
+    {
+        level++;
+    }
+    public void RefreshData()
+    {
+        name.GetComponent<Text>().text = "Nombre: "+mageName;
+        nivel.GetComponent<Text>().text = "Nivel: "+level.ToString();
+        clase.GetComponent<Text>().text = "Clase: "+mageClass;
+        progreso.GetComponent<Text>().text = "Progreso: \n"+progress;
+        realizado.GetComponent<Text>().text = "DMG realizado: \n"+dmgDone.ToString();
+        recibido.GetComponent<Text>().text = "DMG recibido: \n"+dmgReceived.ToString();
+        porcentaje.GetComponent<Text>().text = "Hit% total: \n"+hitP.ToString();
+    }
+
     void Awake()
     {
         DontDestroyOnLoad(this);
