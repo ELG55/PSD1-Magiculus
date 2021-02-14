853f10e6e3c51726fd62167924966caacf5a7b33
Sekler <el_mm5mm@hotmail.com>
2021-02-14 00:49:40 -0700

    Primera versión del mapa.

---
+++ Assets/Scripts/AreaMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AreaMenuController : MonoBehaviour
+{
+    public GameObject areaMenuCanvas;
+    public GameObject areaMenuSubtitle;
+
+    public void menuMostrar(string areaName)
+    {
+        areaMenuSubtitle.GetComponent<Text>().text = areaName;
+        areaMenuCanvas.SetActive(true);
+    }
+
+    public void menuCancelar()
+    {
+        areaMenuCanvas.SetActive(false);
+    }
+
+    public void menuEntrar()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+}
---
+++ Assets/Scripts/BaseMenuController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseMenuController : MonoBehaviour
+{
+    public GameObject baseMenuCanvas;
+
+    public void menuMostrar()
+    {
+        baseMenuCanvas.SetActive(true);
+    }
+
+    public void menuCancelar()
+    {
+        baseMenuCanvas.SetActive(false);
+    }
+
+    public void menuSalir()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void menuOpciones()
+    {
+        //TO BE DONE
+
+        //Add the canvas from the TitleScene when the options window is ready
+
+
+        //TO BE DONE
+    }
+
+    public void menuGuardar()
+    {
+        //TO BE DONE
+
+        //Add the canvas from the TitleScene when the save/load script and window are ready
+
+
+        //TO BE DONE
+    }
+}
