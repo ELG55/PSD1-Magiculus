f183b7f2b7d307827f05dbd9fadce12df8c9d090
EDI URIEL MALDONADO HERNANDEZ <edi_uriel2@hotmail.com>
2021-02-24 22:55:23 -0700

    Musica y sonido agregado. faltan detalles de musica

---
+++ Assets/Scripts/ControllerAudio.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerAudio : MonoBehaviour
+{
+    private AudioSource audioSrc;
+    private static ControllerAudio controllerAudio;
+    //Aqui poner los audios de los distintos botones u ataques
+    /*Ejemplo
+     * public AudioClip SonidoAcierto;
+     */
+    public AudioClip sound1;
+    public AudioClip sound2;
+    public AudioClip Coin;
+    public bool checador = false;
+
+    public Slider SliderSound;
+
+
+    void Awake()
+    {
+        audioSrc = GetComponent<AudioSource>(); //Se asigna el audio que se controlara
+        InicializarVolumen();
+        DontDestroyOnLoad(this);
+
+        if (controllerAudio == null)
+        {
+            controllerAudio = this;
+        }
+        else
+        {
+            Destroy(this.gameObject); // Used Destroy instead of DestroyObject
+        }
+    }
+
+    private void InicializarVolumen() {
+            audioSrc.volume = PlayerPrefs.GetFloat("SoundVolumen", 1.0f);
+            SliderSound.value = audioSrc.volume;
+        
+    }
+    void Update() {
+            audioSrc.volume = SliderSound.value;
+            PlayerPrefs.SetFloat("SoundVolumen", audioSrc.volume);
+            PlayerPrefs.Save();
+    }
+
+    public void PMusic() {
+        audioSrc.PlayOneShot(sound1);
+
+    }
+    public void PSound() {
+        audioSrc.PlayOneShot(sound2);
+    }
+
+    public void coin() {
+        audioSrc.PlayOneShot(Coin);
+    }
+
+    /*Crear un metodo para reproducir ese sonido especifico
+      public void Acierto(){
+           audioSrc.clip = SonidoAcierto;
+           audioScr.play();
+      }
+     
+    */
+
+}
---
+++ Assets/Scripts/ControllerAudioMusic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerAudioMusic : MonoBehaviour
+{
+
+    private AudioSource MusicSrc;
+    public AudioClip Music;
+    public Slider SliderMusic;
+    private static ControllerAudioMusic controladorMusica;
+    //Aqui agregan mas si quieren mas musicas
+    //public AudioClip Music1;
+    //public AudioClip Music2;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MusicSrc = GetComponent<AudioSource>(); //Se asigna el audio que se controlara
+        InicializarVolumenMusic();
+        MusicSrc.Play();
+
+        DontDestroyOnLoad(this);
+
+        if (controladorMusica == null)
+        {
+            controladorMusica = this;
+        }
+        else
+        {
+            Destroy(this.gameObject); // Used Destroy instead of DestroyObject
+        }
+
+
+    }
+    private void InicializarVolumenMusic()
+    {
+        MusicSrc.volume = PlayerPrefs.GetFloat("MusicVolumen", 1.0f);
+        SliderMusic.value = MusicSrc.volume;
+    }
+    void Update()
+    {
+        MusicSrc.volume = SliderMusic.value;
+        PlayerPrefs.SetFloat("MusicVolumen", MusicSrc.volume);
+        PlayerPrefs.Save();
+    }
+
+
+    // Update is called once per frame
+
+
+}
